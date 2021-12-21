12adf36adf5499872899bb1214aa1d9053277b4d
yannick97 <yannick.kuh@gmail.com>
2021-12-21 16:54:58 +0100

    Speed field removed from shark.

--- Boid Test/Assets/Scripts/Predator.cs
+++ Boid Test/Assets/Scripts/Predator.cs
@@ -2,12 +2,10 @@ using Unity.Entities;
 
 [GenerateAuthoringComponent]
 public struct Predator : IComponentData
-{
-    public float Speed;
-
+{ 
     public float MaxAcceleration;
     public float MaxSpeed;
     public float MinSpeed;
     public float HuntingAcceleration;
-    public float DistanceToPrey;
+    public float HuntingRadius;
 }
